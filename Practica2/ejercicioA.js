0bca4f4483188e6f5252f1619bd6b94ce6d3f795
Alvaro-Ochoa-Reza <124049960@upq.edu.mx>
2025-09-21 21:50:53 -0600

    practica2 ejerA

---
+++ Practica2/ejercicioA.js
@@ -0,0 +1,12 @@
+const persona = {
+  nombre: "Alvaro",
+  edad: 21,
+  direccion: {
+    ciudad: "Querétaro",
+    pais: "México"
+  }
+};
+
+const { nombre, edad, direccion: { ciudad, pais } } = persona;
+
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + "," + pais + '.');
