70be9f30fed6dac0cba941eaae03655763bcbc38
Alvaro-Ochoa-Reza <124049960@upq.edu.mx>
2025-10-08 08:51:38 -0600

    commit PracticNo5

--- Intro/App.js
+++ Intro/App.js
@@ -1,19 +1,26 @@
 // 1.Imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
+import React,{useState} from 'react';
 
 //2. Main: Zona de componentes
 export default function App() {
-  const[contador,setContador]=useState(0);
+
+    const [contador,setContador]= useState(0);
+
   return (
+
     <View style={styles.container}>
+
       <Text> Contador: {contador} </Text>
-      <Button title='Agregar' onPress={()=>setContador(contador+1)}/>
-      <Button title='Quitar' onPress={()=>setContador(contador-1)}/> 
-      <Button title='Reiniciar' onPress={()=>setContador(0)}/>   
+      <Buttom title='Agregar' onPress={()=>setContador(contador+1)} />
+      <Buttom title='Quitar' onPress={()=>setContador(contador-1)} />
+      <Buttom title='Reiniciar' onPress={()=>setContador(0)} />
       <StatusBar style="auto" />
+
     </View>
+
+
   );
 }
 
