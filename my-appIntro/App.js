e0094ffc53005d375bd8250f6a327e1048b5e402
Alvaro-Ochoa-Reza <124049960@upq.edu.mx>
2025-10-13 18:38:59 -0600

    clase 13-10-25

--- my-appIntro/App.js
+++ my-appIntro/App.js
@@ -1,11 +1,12 @@
 // 1.Imports: Zona de importaciones
-import ContadorScreens from "./screens/ContadorScreens";
+import MenuScreen from "./screens/MenuScreen";
+
 //2. Main: Zona de componentes
 export default function App() {
 
   return (
 
-    <ContadorScreens> </ContadorScreens>
+    <MenuScreen></MenuScreen>
 
   );
 }
---
+++ my-appIntro/screens/MenuScreen.js
@@ -0,0 +1,26 @@
+import { Text, StyleSheet, View, Buttom } from 'react-native'
+import React, { useState} from 'react'
+import BotonesScreen from './BotonesScreen';
+import ContadorScreen from './ContadorScreens';
+
+export default function MenuScreen() {
+    const [screen,setScreen]= useState('menu');
+
+    switch(screen){
+        case'contador':
+            return <ContadorScreen/>;
+        case'botones':
+            return <BotonesScreen/>;
+        case'menu':
+            default:
+            return (
+                <View>
+                    <Text> Menu de Practicas </Text>
+                    <Buttom onPress={()=>setScreen('contador')} title='Pract:contador'></Buttom>
+                    <Buttom onPress={()=>setScreen('botones')} title='Pract:botones'></Buttom>
+                </View>
+        ) 
+    }
+
+}
+const styles = StyleSheet.create({})
