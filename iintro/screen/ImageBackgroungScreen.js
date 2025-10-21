c72cc3316f4893cbbbbbf7d8ad26939c02d65eec
Alvaro-Ochoa-Reza <124049960@upq.edu.mx>
2025-10-21 17:30:06 -0600

    mageBackgroung & SlapshScreen: Completada

--- iintro/screen/ImageBackgroungScreen.js
+++ iintro/screen/ImageBackgroungScreen.js
@@ -1,25 +1,74 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
-export default function ImageBackgroundScreen() {
+const backgroundImage = require('../assets/Baki.webp');
+
+export default function ImageBackgroundScreen({ navigation }) {
     
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 10000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
     return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Próximamente: ImageBackgroung & SlapshScreen</Text>
+      <ImageBackground 
+        source={backgroundImage}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.title}> Bienvenido </Text>
+          <Text style={styles.title}> Cargando... </Text>
+        </View>
+      </ImageBackground>
+    )
+  }
+  return (
+    <ImageBackground 
+        source={backgroundImage}
+        style={styles.background}
+        resizeMode="cover"
+    >
+    <View style={styles.mainScreen}>
+      <Text style={styles.mainText}>Bienvenido a la pantalla principal</Text>
     </View>
+    </ImageBackground>
   );
-}
+}  
+
+const {width, heigth} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    width: width,
+    height: heigth,
+  },  
+  overlay: {
     flex: 1,
+    backgroundColor: '#71756f7a',
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
   },
-  texto: {
-    fontSize: 24,
+  title: {
+    color: '#FFF',
+    fontSize: 32,
     fontWeight: 'bold',
+    marginBottom: 10,
     textAlign: 'center',
+  },
+  mainScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f202',
+  },
+  mainText: {
+    fontSize: 24,
+    color: '#ffffffff',
   },
 });
